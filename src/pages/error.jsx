555773ecd22d88ed203e9805d42c9f3b413a7806
ThatOwlGuy <theOwlInbox@gmail.com>
2022-10-31 13:24:05 -0500

    put in placeholder pages

---
+++ src/pages/error.jsx
@@ -0,0 +1,9 @@
+export default function Error() {
+    return (
+        <div>
+            <center>
+                <h1>Something went wrong! Try clicking <a href="home">here!</a> to reload the app or try again later.</h1>
+            </center>
+        </div>
+    );
+}
