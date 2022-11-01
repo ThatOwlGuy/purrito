28d70ad73826733591e6ea3aac8ec67cd42ca8b4
ThatOwlGuy <theOwlInbox@gmail.com>
2022-11-01 18:24:43 -0500

    setup nonfunctional api and json mock database

---
+++ src/api/PurritoPurchaseService.js
@@ -0,0 +1,9 @@
+const apiUrl = "http://localhost:3001/orders";
+
+export async function PurchasePurritos(purritoOrder) {
+    let response = await fetch(apiUrl);
+}
+
+export async function GetDeliveryEstimate(orderID) {
+    
+}
