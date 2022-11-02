8a0f58ca98d7c97190b6cfb280604d2f634bba78
Sebastian-Mac <theOwlInbox@gmail.com>
2022-11-02 11:05:29 -0500

    Progressing show current order
    Probably going to switch to universal-cookies instead of react-cookies to handle the in cart items

--- src/pages/burritoMaker/burritoMaker.jsx
+++ src/pages/burritoMaker/burritoMaker.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Cookies } from "react-cookie";
+import { instanceOf } from 'prop-types';
 import burritoPic from "../../assets/images/burrito.jpg";
 import { PurchasePurritos } from "../../api/PurritoPurchaseService";
 
@@ -77,119 +78,125 @@ export default class PurritoPurchaser extends React.Component {
         this.setState({currentProtein : purrito});
     }
 
-    render() {
-        return (
-            <div>
-                <div className="flex-container">
-                    <img src={burritoPic} className="purrito-banner" />
-                    <div className="info-panel" style={{padding:"1rem", float:"right"}}>
-                        <h2>1 Large Purrito</h2>
-                        <h3>$10.99</h3>
-                    </div>
-                </div>
-                <div className="widget-panel">
-                    <h2><center>
-                        The standard large purrito comes with your choice of protein, 
-                        tortilla, rice, peppers, onions, & cheese!
-                    </center></h2>
-                    <div id="burrito-options" className="info-panel" style={{display:"flex", justifyContent:"space-evenly"}}>
-                        <div>
-                            <div>
-                                <h1>Protein</h1>
-                                <h3>
-                                    <input type="radio" value="Beef" name="Protein" /> Beef
-                                </h3>
-                                <h3>
-                                    <input type="radio" value="Chicken" name="Protein" /> Chicken
-                                </h3>
-                                <h3>
-                                    <input type="radio" value="Carne Asada" name="Protein" /> Carne Asada
-                                </h3>
-                                <h3>
-                                    <input type="radio" value="Black Beans" name="Protein" /> Black Beans
-                                </h3>
-                            </div>
-                            <div>
-                                <h1>Tortilla</h1>
-                                <h3>
-                                    <input type="radio" value="Jalapeno Tortilla" name="Tortilla" /> Jalapeno Tortilla
-                                </h3>
-                                <h3>
-                                    <input type="radio" value="Cholula Tortilla" name="Tortilla" /> Cholula Tortilla
-                                </h3>
-                                <h3>
-                                    <input type="radio" value="Maiz Tortilla" name="Tortilla" /> Maiz Tortilla
-                                </h3>
-                                <h3>
-                                    <input type="radio" value="Blue Corn Tortilla" name="Tortilla" /> Blue Corn Tortilla
-                                </h3>
-                            </div>
-                            <div>
-                                <h1>Rice</h1>
-                                <h3>
-                                    <input type="radio" value="White" name="Rice" /> White
-                                </h3>
-                                <h3>
-                                    <input type="radio" value="Brown" name="Rice" /> Brown
-                                </h3>
-                                <h3>
-                                    <input type="radio" value="No Rice" name="Rice" /> No Rice
-                                </h3>
-                            </div>
-                        </div>
-                        <div>
-                            <div>
-                                <h1>Peppers & Onions</h1>
-                                <h3>
-                                    <input type="radio" value="None" name="Peppers & Onions" /> None
-                                </h3>
-                                <h3>
-                                    <input type="radio" value="Peppers Only" name="Peppers & Onions" /> Peppers Only
-                                </h3>
-                                <h3>
-                                    <input type="radio" value="Onions Only" name="Peppers & Onions" /> Onions Only
-                                </h3>
-                                <h3>
-                                    <input type="radio" value="Peppers & Onions" name="Peppers & Onions" /> Peppers & Onions
-                                </h3>
-                            </div>
-                            <div>
-                                <h1>Cheese</h1>
-                                <h3>
-                                    <input type="radio" value="Monterrey Jack" name="Cheese" /> Monterrey Jack
-                                </h3>
-                                <h3>
-                                    <input type="radio" value="Queso Blanco" name="Cheese" /> Queso Blanco
-                                </h3>
-                                <h3>
-                                    <input type="radio" value="Cotija" name="Cheese" /> Cotija
-                                </h3>
-                                <h3>
-                                    <input type="radio" value="No Cheese" name="Cheese" /> No Cheese
-                                </h3>
-                            </div>
-                            <div>
-                                <h1 style={{marginBottom:"0rem"}}>Include Cat-Friendly Purrito</h1>
-                                <p style={{marginTop:"0rem"}}>(Rice paper tortilla, fish, and nip seasoning)</p>
-                                <h3>
-                                    <input type="radio" value="Monterrey Jack" name="Cheese" /> Give my cat a purrito!
-                                </h3>
-                                <h3>
-                                    <input type="radio" value="No Cheese" name="Cheese" /> No purrito
-                                </h3>
-                            </div>
-                        </div>
-                    </div>
-                    <center style={{marginTop:"0rem", display:"flex", justifyContent:"space-evenly"}}>
-                        <button className="info-panel">
-                            <h1 style={{margin:"0.1rem"}}>¡Add Purrito-Burrito!</h1>
-                        </button>
-                        <button className="info-panel">
-                            <h1 style={{margin:"0.1rem"}}>¡Purchase Purrito-Burrito(s)!</h1>
-                        </button>
-                    </center>
+    BurritoMaker = () => (
+        <div>
+            <div className="flex-container">
+                <img src={burritoPic} className="purrito-banner" />
+                <div className="info-panel" style={{padding:"1rem", float:"right"}}>
+                    <h2>1 Large Purrito</h2>
+                    <h3>$10.99</h3>
                 </div>
             </div>
+            <div className="widget-panel">
+                <h2><center>
+                    The standard large purrito comes with your choice of protein, 
+                    tortilla, rice, peppers, onions, & cheese!
+                </center></h2>
+                <div id="burrito-options" className="info-panel" style={{display:"flex", justifyContent:"space-evenly"}}>
+                    <div>
+                        <div>
+                            <h1>Protein</h1>
+                            <h3>
+                                <input type="radio" value="Beef" name="Protein" /> Beef
+                            </h3>
+                            <h3>
+                                <input type="radio" value="Chicken" name="Protein" /> Chicken
+                            </h3>
+                            <h3>
+                                <input type="radio" value="Carne Asada" name="Protein" /> Carne Asada
+                            </h3>
+                            <h3>
+                                <input type="radio" value="Black Beans" name="Protein" /> Black Beans
+                            </h3>
+                        </div>
+                        <div>
+                            <h1>Tortilla</h1>
+                            <h3>
+                                <input type="radio" value="Jalapeno Tortilla" name="Tortilla" /> Jalapeno Tortilla
+                            </h3>
+                            <h3>
+                                <input type="radio" value="Cholula Tortilla" name="Tortilla" /> Cholula Tortilla
+                            </h3>
+                            <h3>
+                                <input type="radio" value="Maiz Tortilla" name="Tortilla" /> Maiz Tortilla
+                            </h3>
+                            <h3>
+                                <input type="radio" value="Blue Corn Tortilla" name="Tortilla" /> Blue Corn Tortilla
+                            </h3>
+                        </div>
+                        <div>
+                            <h1>Rice</h1>
+                            <h3>
+                                <input type="radio" value="White" name="Rice" /> White
+                            </h3>
+                            <h3>
+                                <input type="radio" value="Brown" name="Rice" /> Brown
+                            </h3>
+                            <h3>
+                                <input type="radio" value="No Rice" name="Rice" /> No Rice
+                            </h3>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <h1>Peppers & Onions</h1>
+                            <h3>
+                                <input type="radio" value="None" name="Peppers & Onions" /> None
+                            </h3>
+                            <h3>
+                                <input type="radio" value="Peppers Only" name="Peppers & Onions" /> Peppers Only
+                            </h3>
+                            <h3>
+                                <input type="radio" value="Onions Only" name="Peppers & Onions" /> Onions Only
+                            </h3>
+                            <h3>
+                                <input type="radio" value="Peppers & Onions" name="Peppers & Onions" /> Peppers & Onions
+                            </h3>
+                        </div>
+                        <div>
+                            <h1>Cheese</h1>
+                            <h3>
+                                <input type="radio" value="Monterrey Jack" name="Cheese" /> Monterrey Jack
+                            </h3>
+                            <h3>
+                                <input type="radio" value="Queso Blanco" name="Cheese" /> Queso Blanco
+                            </h3>
+                            <h3>
+                                <input type="radio" value="Cotija" name="Cheese" /> Cotija
+                            </h3>
+                            <h3>
+                                <input type="radio" value="No Cheese" name="Cheese" /> No Cheese
+                            </h3>
+                        </div>
+                        <div>
+                            <h1 style={{marginBottom:"0rem"}}>Include Cat-Friendly Purrito</h1>
+                            <p style={{marginTop:"0rem"}}>(Rice paper tortilla, fish, and nip seasoning)</p>
+                            <h3>
+                                <input type="radio" value="Monterrey Jack" name="Cheese" /> Give my cat a purrito!
+                            </h3>
+                            <h3>
+                                <input type="radio" value="No Cheese" name="Cheese" /> No purrito
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <center style={{marginTop:"0rem", display:"flex", justifyContent:"space-evenly"}}>
+                    <button className="info-panel">
+                        <h1 style={{margin:"0.1rem"}}>¡Add Purrito-Burrito!</h1>
+                    </button>
+                    <button className="info-panel">
+                        <h1 style={{margin:"0.1rem"}}>¡Purchase Purrito-Burrito(s)!</h1>
+                    </button>
+                </center>
+            </div>
+        </div>
+    )
+
+    render() {
+        return (
+            <>
+                {this.BurritoMaker}
+            </>
         );
     }
 }
--- src/pages/burritoMaker/inCart.jsx
+++ src/pages/burritoMaker/inCart.jsx
@@ -1,9 +1,35 @@
-export default function InCart() {
-    return (
+export default class InCart extends React.Component {
+    static propTypes = {
+        cookies: instanceOf(Cookies).isRequired
+    };
+
+    constructor(props) {
+        super(props);
+        const { cookies } = props;
+        this.state = {
+            inCart : cookies.get('inCart') || null
+        };
+    }
+
+    items = () => {
+        const { cookies } = props;
+        let itemsInCart = mockCart || cookies.get('inCart');
+        return itemsInCart;
+    };
+
+    cartList = this.items.map((item, index) => {
+        return (
+            <li key={index} data-index={index}>
+                <h3>1 x {item.protein} Burrito</h3>
+            </li>
+        );
+    });
+
+    render() {
         <div>
-            
+            {this.cartList}
         </div>
-    );
+    }
 }
 
 var mockCart = {
