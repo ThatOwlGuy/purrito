de1d35de3dc043fd6461393570cd16f455331738
ThatOwlGuy <theOwlInbox@gmail.com>
2022-11-01 20:47:23 -0500

    setup boiler plate for purrito purchase

--- src/api/PurritoPurchaseService.js
+++ src/api/PurritoPurchaseService.js
@@ -1,9 +1,11 @@
 const apiUrl = "http://localhost:3001/orders";
 
 export async function PurchasePurritos(purritoOrder) {
+    // Create id for order and save as cookie
     let response = await fetch(apiUrl);
+
 }
 
 export async function GetDeliveryEstimate(orderID) {
-    
+
 }
