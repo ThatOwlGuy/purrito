5535d6ebb16b34f2d47c3e553572087ae9670b50
Sebastian-Mac <theOwlInbox@gmail.com>
2022-11-02 09:19:50 -0500

    started in cart component

---
+++ src/pages/burritoMaker/inCart.jsx
@@ -0,0 +1,36 @@
+export default function InCart() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+var mockCart = {
+    burritos : [
+        {
+            protein : "beef",
+            tortilla : "flour tortilla",
+            rice : "white",
+            peppersAndOnions : "peppers",
+            cheese : "cotija",
+            purrito : true
+        },
+        {
+            protein : "chicken",
+            tortilla : "flour tortilla",
+            rice : "white",
+            peppersAndOnions : "peppers & onions",
+            cheese : "chihuahua",
+            purrito : false
+        },
+        {
+            protein : "black beans",
+            tortilla : "flour tortilla",
+            rice : "brown",
+            peppersAndOnions : "onions",
+            cheese : "monterrey jack",
+            purrito : false
+        },
+    ]
+}
